32a092fe929f663072a63430138b9fbf36fb32f5
unknown <Margalite@.(none)>
2011-07-25 22:13:00 +0300

    Fixed some problems in the init code. Voting works, but the radar doesn't work due to a bug in it's init.

--- src/js/votes.js
+++ src/js/votes.js
@@ -1,4 +1,5 @@
-var votes = {
+var votes = {};
+votes = {
     i: 0,
     user_votes: [],
     votes_data: [],
@@ -22,26 +23,28 @@ var votes = {
         }
         $.get("http://oknesset.org/api/vote/"+vote_id+"/", callback, "jsonp");  
     },
-    render_qs: function() {
-        var t = $('#questions_template').html();
-        $('#questions').html(Mustache.to_html(t, {questions:votes.votes_data}));
-        $('.button').click(function() {
-            var vote = vote_values[$(this).children().attr("class")];
-            votes.user_votes[votes.i]=vote;
-            $('#question-'+votes.votes_data[votes.i].id).hide();
-            votes.i++;
-            $('#question-'+votes.votes_data[votes.i].id).show();
-            update_mks(vote)
-
-        });
-    },
     update_mks: function(user_vote) {
-        for (var j=0; j < data.mv.length(); j++) {
+        for (var j=0; j < data.mv.length; j++) {
             var mk_id = data.mi[j];
             var mk_vote = data.mv[j][votes.i];
             if (user_vote != 0 ) {
                 votes.mks_cor[mk_id] = votes.mks_cor[mk_id] + 2 - (mk_vote-user_vote)*(mk_vote-user_vote);
             }
         }
+    },
+	
+    render_qs: function() {
+        var t = $('#questions_template').html();
+        $('#questions').html(Mustache.to_html(t, {questions:votes.votes_data}));
+        $('.button').click(function() {
+            var vote = votes.vote_values[$(this).children().attr("class")];
+            votes.user_votes[votes.i]=vote;
+            $('#question-'+votes.votes_data[votes.i].id).hide();
+            votes.i++;
+            $('#question-'+votes.votes_data[votes.i].id).show();
+            votes.update_mks(vote)
+
+        });
     }
+
 }
